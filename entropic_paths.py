4ae6dbb5526459e632ead4c771f51e3d942dfaec
malik22 <malik22@pal-nat186-173-145.itap.purdue.edu>
2023-04-19 17:39:39 -0400

    updated for stuff

--- entropic_paths.py
+++ entropic_paths.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from typing import Dict, Optional, Set
 from math import log2
 
@@ -9,58 +11,99 @@ class Branch:
     def __repr__(self) -> str:
         return f'Branch({self.cost}, {self.probability:.2f})'
 
+class Label:
+    def __init__(self, lbl: str):
+        self.lbl = lbl
 
-Tree = Dict[int, Branch]
+    def __repr__(self) -> str:
+        return self.lbl
+class Tree:
+    def __init__(self, branch: Branch, left: Tree | Label, right: Tree | Label):
+        self.branch = branch
+        self.left = left
+        self.right = right
 
-def make_tree(index: int, depth: int, cur: Tree = {}):
-    cur[index] = Branch()
+    def __repr__(self) -> str:
+        return f'Tree({self.branch}, {self.left}, {self.right})'
 
-    if depth == 0:
-        return cur
+Node = Tree | Label
+SerTree = Dict[int, Branch]
 
-    cur = make_tree(index * 2, depth - 1, cur)
-    cur = make_tree(index * 2 + 1, depth - 1, cur)
-    return cur
+def serialize(tree: Node) -> SerTree:
+    serialized: SerTree = {}
+    if isinstance(tree, Label):
+        return serialized
+        
+    def visit(node: Node, index: int):
+        if isinstance(node, Label):
+            return
+        serialized[index] = node.branch
+        visit(node.left, index * 2)
+        visit(node.right, index * 2 + 1)
+
+    visit(tree, 1)
+    return serialized
 
 
-def visiting_probabilities(tree: Tree, node: int, acc: float, probabilities: Dict[int, float] = {}):
-    probabilities[node] = acc
-    if node * 2 in tree:
-        visiting_probabilities(tree, node * 2, tree[node].probability * acc, probabilities)
+def make_tree(depth: int) -> Tree:
+    left = make_tree(depth - 1)
+    right = make_tree(depth - 1)
+    return Tree(Branch(), left, right)
+
+# def make_tree_old(index: int, depth: int, cur: Tree):
+
+#     cur[index] = Branch()
+
+#     if depth == 0:
+#         return cur
+
+#     cur = make_tree(index * 2, depth - 1, cur)
+#     cur = make_tree(index * 2 + 1, depth - 1, cur)
+#     return cur
+
+
+
+# def visiting_probabilities(tree: Tree, node: int, acc: float, probabilities: Dict[int, float] = {}):
+#     probabilities[node] = acc
+#     if node * 2 in tree:
+#         visiting_probabilities(tree, node * 2, tree[node].probability * acc, probabilities)
     
-    if node * 2 + 1 in tree:
-        visiting_probabilities(tree, node * 2 + 1, (1 - tree[node].probability) * acc, probabilities)
+#     if node * 2 + 1 in tree:
+#         visiting_probabilities(tree, node * 2 + 1, (1 - tree[node].probability) * acc, probabilities)
 
 
-def bits_leaked(tree: Tree, slice: Set[int], root: int = 1) -> float:
-    probabilities: Dict[int, float] = {}
-    visiting_probabilities(tree, root, 1, probabilities)
+# def bits_leaked(tree: Tree, slice: Set[int], root: int = 1) -> float:
+#     probabilities: Dict[int, float] = {}
+#     visiting_probabilities(tree, root, 1, probabilities)
 
-    print(probabilities)
+#     print(probabilities)
 
-    entropy = 0
-    for node in slice:
-        entropy -= probabilities[node] * log2(probabilities[node])
+#     entropy = 0
+#     for node in slice:
+#         entropy -= probabilities[node] * log2(probabilities[node])
 
-    return entropy
+#     return entropy
 
 
-def tree_cost(tree: Tree, root: int) -> int:
+# def tree_cost(tree: Optional[Tree]) -> int:
+#     if tree is None:
+#         return 0
+#     return tree.branch.cost + tree_cost(tree.left) + tree_cost(tree.right)
+
+def tree_cost(tree: SerTree, root: int) -> int:
     if root not in tree:
         return 0
 
     return tree[root].cost + tree_cost(tree, root * 2) + tree_cost(tree, root * 2 + 1)
 
-
-
-def all_slices(tree: Tree, cur=set()):
+def all_slices(tree: SerTree, cur=set()):
     yield cur
     for val in cur:
         if val * 2 in tree and val * 2 + 1 in tree:
             yield from all_slices(tree, cur - {val} | {val * 2, val * 2 + 1})
 
 
-def slicing_cost(tree: Tree, slice: Set[int]):
+def slicing_cost(tree: SerTree, slice: Set[int]):
     probs: Dict[int, float] = {}
     def visit(node: int, acc: float = 1):
         if node not in tree:
@@ -72,9 +115,9 @@ def slicing_cost(tree: Tree, slice: Set[int]):
 
     visit(1)
 
-    cost = 0
+    cost = 0.0
+    entropy = 0.0
 
-    entropy = 0
     for node in slice:
         if node not in probs:
             raise Exception(f'Invalid slice: ill-defined ({node})')
@@ -88,7 +131,7 @@ def slicing_cost(tree: Tree, slice: Set[int]):
 #     print(slice, slicing_cost(tree, slice))
 
 if __name__ == '__main__':
-    gcd_tree = {}
+    gcd_tree: SerTree = {}
     gcd_tree[1] = Branch(0)
     gcd_tree[2] = Branch(1)
     gcd_tree[3] = Branch(1)
